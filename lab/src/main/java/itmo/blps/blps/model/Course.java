58dff03ef8a7ae0cdcdb495c51cb49ba62ca5a4a
ta4ilka69 <artem_balin_2004@mail.ru>
2025-02-17 16:29:48 +0300

    init mvn project

---
+++ lab/src/main/java/itmo/blps/blps/model/Course.java
@@ -0,0 +1,24 @@
+package itmo.blps.blps.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean available;
+
+    @Column(name = "min_score")
+    private Integer minimumScore;
+} 
---
+++ lab/src/main/java/itmo/blps/blps/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package itmo.blps.blps.repository;
+
+import itmo.blps.blps.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+} 
