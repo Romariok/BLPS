dd9446bbc537452097dbbe4862b8b93c13ee6b9b
ta4ilka69 <artem_balin_2004@mail.ru>
2025-02-26 15:49:12 +0300

    400 codes instread of 200 on exception

--- lab/src/main/java/itmo/blps/blps/exception/GlobalExceptionHandler.java
+++ lab/src/main/java/itmo/blps/blps/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package itmo.blps.blps.exception;
 
 import itmo.blps.blps.dto.EnrollmentResponseDTO;
 import itmo.blps.blps.dto.TaskSubmissionResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,11 +12,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CourseEnrollmentException.class)
     public ResponseEntity<EnrollmentResponseDTO> handleCourseEnrollmentException(CourseEnrollmentException ex) {
-        return ResponseEntity.ok(new EnrollmentResponseDTO(false, ex.getMessage()));
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new EnrollmentResponseDTO(false, ex.getMessage()));
     }
 
     @ExceptionHandler(TaskOperationException.class)
     public ResponseEntity<TaskSubmissionResponseDTO> handleTaskOperationException(TaskOperationException ex) {
-        return ResponseEntity.ok(new TaskSubmissionResponseDTO(false, ex.getMessage(), null));
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new TaskSubmissionResponseDTO(false, ex.getMessage(), null));
     }
 }
