718f08d62c0a2782dde778da1872ebf7d5f2a574
ta4ilka69 <artem_balin_2004@mail.ru>
2025-04-21 22:47:52 +0300

    some activemq start

---
+++ CumUnit/blps/src/main/java/itmo/blps/ComputationApplication.java
@@ -0,0 +1,13 @@
+package itmo.blps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputationApplication.class, args);
+	}
+
+}
