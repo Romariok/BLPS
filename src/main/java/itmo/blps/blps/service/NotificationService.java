908996185db2ff1348c1bb45c09ba4e251c7731e
ta4ilka69 <artem_balin_2004@mail.ru>
2025-04-21 12:32:29 +0300

    startnig scheduling

---
+++ src/main/java/itmo/blps/blps/service/NotificationService.java
@@ -0,0 +1,10 @@
+package itmo.blps.blps.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+    @Scheduled(fixedRate = )
+
+}
