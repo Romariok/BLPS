04d6773715778d912ad08806c967fb39337a7c5b
ta4ilka69 <artem_balin_2004@mail.ru>
2025-05-11 15:38:00 +0300

    trying cert request bpmn

--- src/main/java/itmo/blps/blps/bpms/CertificateServiceBpms.java
+++ src/main/java/itmo/blps/blps/bpms/CertificateServiceBpms.java
@@ -1,12 +1,79 @@
 package itmo.blps.blps.bpms;
 
+import java.util.Set;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import itmo.blps.blps.model.CertificateRequest;
+import itmo.blps.blps.model.Permission;
+import itmo.blps.blps.repository.CertificateRequestRepository;
+import itmo.blps.blps.repository.CourseRepository;
+import itmo.blps.blps.repository.TaskSubmissionRepository;
+import itmo.blps.blps.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service("certServ")
 public class CertificateServiceBpms {
     private static final float threshold = 0.6f;
-    private final CourseRepository courseRepository;
-    private final UserRepository userRepository;
-    private final TaskSubmissionRepository submissionRepository;
-    private final CertificateRequestRepository certificateRequestRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskSubmissionRepository submissionRepository;
+    @Autowired
+    private CertificateRequestRepository certificateRequestRepository;
+
+    public void isExistingCourse(Long courseId) {
+        if(!courseRepository.existsById(courseId)){
+            throw new BpmnError("404");
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    public void checkAuthority(DelegateExecution execution){
+        Set<String> authorities = (Set<String>) execution.getVariable("authorities");
+        if(authorities == null || !authorities.contains(Permission.VIEW_CERTIFICATE.toString())){
+            log.info("\n\n\n\n\nAUTHORITIES: ");
+            if (authorities != null) {
+                authorities.forEach(auth -> log.info(auth));
+            }
+            log.info("\n\n\n\n\n");
+            throw new BpmnError("403");
+        }
+    }
+
+    public boolean areAllTaskDone(Long courseId, Long userId) {
+        boolean allTasksCompleted = courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Unchecked existance of course"))
+                .getTasks()
+                .stream()
+                .allMatch(task -> {
+                    var submission = submissionRepository
+                            .findTopByStudentIdAndTaskIdOrderBySubmittedAtDesc(userId, task.getId())
+                            .orElse(null);
+
+                    return submission != null && submission.getScore() != null
+                            && submission.getScore() >= task.getMaxScore() * threshold;
+                });
+        return allTasksCompleted;
+    }
+
+    public void createCertificateRequest(Long userId, Long courseId) {
+        var course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Unchecked existance of course"));
+
+        var student = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Unchecked existance of user"));
+        CertificateRequest certificateRequest = new CertificateRequest();
+        certificateRequest.setCourse(course);
+        certificateRequest.setStudent(student);
+        certificateRequest.setRequestedAt(java.time.LocalDateTime.now());
+        certificateRequest.setStatus(itmo.blps.blps.model.CertificateRequestStatus.IN_PROGRESS);
+        log.info("Certificate request created: {}", certificateRequest);
+    }
 }
