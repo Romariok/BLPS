e125580de737aecf88c0a8433a5940aae70258b5
Romariok <ronya-ko@yandex.ru>
2025-05-17 00:07:34 +0300

    add all certificate bp

--- src/main/java/itmo/blps/blps/bpms/CertificateServiceBpms.java
+++ src/main/java/itmo/blps/blps/bpms/CertificateServiceBpms.java
@@ -1,6 +1,7 @@
 package itmo.blps.blps.bpms;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -14,6 +15,10 @@ import itmo.blps.blps.repository.CertificateRequestRepository;
 import itmo.blps.blps.repository.CourseRepository;
 import itmo.blps.blps.repository.TaskSubmissionRepository;
 import itmo.blps.blps.repository.UserRepository;
+import itmo.blps.blps.service.JmsProducerService;
+import itmo.blps.blps.dto.CertificateRequestJmsDTO;
+import itmo.blps.blps.dto.CertificateRequestResponseDTO;
+import itmo.blps.blps.mapper.CertificateMapper;
 
 @Service("certServ")
 public class CertificateServiceBpms {
@@ -26,18 +31,48 @@ public class CertificateServiceBpms {
     private TaskSubmissionRepository submissionRepository;
     @Autowired
     private CertificateRequestRepository certificateRequestRepository;
+    @Autowired
+    private CertificateMapper certificateMapper;
+    @Autowired
+    private JmsProducerService jmsProducerService;
+
 
     public void isExistingCourse(Long courseId) {
         if (!courseRepository.existsById(courseId)) {
             throw new BpmnError("404");
         }
-        ;
+    }
+
+    public void isExistingRequest(Long courseId, Long userId) {
+        if (!certificateRequestRepository.existsByStudentIdAndCourseId(userId, courseId)) {
+            throw new BpmnError("404");
+        }
+    }
+
+    public void isExistingRequest(Long requestId) {
+        if (!certificateRequestRepository.existsById(requestId)) {
+            throw new BpmnError("404");
+        }
+    }
+
+    public void isRequestInProgress(Long requestId) {
+        if (certificateRequestRepository.findById(requestId).get().getStatus() != itmo.blps.blps.model.CertificateRequestStatus.IN_PROGRESS) {
+            throw new BpmnError("400");
+        }
     }
 
     @SuppressWarnings("unchecked")
     public void checkAuthority(DelegateExecution execution) {
         Set<String> authorities = (Set<String>) execution.getVariable("authorities");
         if (authorities == null || !authorities.contains(Permission.VIEW_CERTIFICATE.toString())) {
+            throw new BpmnError("403");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void checkTeacherAuthority(DelegateExecution execution) {
+        Set<String> authorities = (Set<String>) execution.getVariable("authorities");
+        if (authorities == null || !authorities.contains(Permission.ISSUE_CERTIFICATE.toString())) {
             throw new BpmnError("403");
         }
     }
@@ -71,4 +106,43 @@ public class CertificateServiceBpms {
         certificateRequest.setStatus(itmo.blps.blps.model.CertificateRequestStatus.IN_PROGRESS);
         certificateRequestRepository.save(certificateRequest);
     }
+
+    public String getPendingRequests(Long courseId) {
+        return certificateRequestRepository
+                        .findByCourseIdAndStatus(courseId, itmo.blps.blps.model.CertificateRequestStatus.IN_PROGRESS)
+                        .stream()
+                        .map(certificateMapper::toCertificateRequestListDTO)
+                        .collect(Collectors.toList()).toString();
+    }
+
+    public String checkRequestStatus(Long userId, Long courseId) {
+        var request = certificateRequestRepository.findByStudentIdAndCourseId(userId, courseId)
+                .orElseThrow(() -> new BpmnError("404"));
+
+        return request.getStatus().toString();
+    }
+
+    public void requestCertificateGeneration(Long userId, Long courseId) {
+        CertificateRequestJmsDTO requestDto = new CertificateRequestJmsDTO();
+        requestDto.setUserId(userId);
+        requestDto.setCourseId(courseId);
+        jmsProducerService.sendMessage(requestDto);
+    }
+
+    public String processCertificateRequest(Boolean decision, Long requestId) {
+        CertificateRequest request = certificateRequestRepository.findById(requestId)
+                        .orElseThrow(() -> new BpmnError("404"));
+
+        if (request.getStatus() != itmo.blps.blps.model.CertificateRequestStatus.IN_PROGRESS) {
+                throw new BpmnError("400");
+        }
+
+        request.setStatus(decision
+                        ? itmo.blps.blps.model.CertificateRequestStatus.APPROVED
+                        : itmo.blps.blps.model.CertificateRequestStatus.REJECTED);
+
+        certificateRequestRepository.save(request);
+        requestCertificateGeneration(request.getStudent().getId(),request.getCourse().getId());
+        return decision ? "Certificate request approved" : "Certificate request rejected";
+}
 }
--- src/main/java/itmo/blps/blps/dto/CertificateRequestListDTO.java
+++ src/main/java/itmo/blps/blps/dto/CertificateRequestListDTO.java
@@ -1,13 +1,24 @@
 package itmo.blps.blps.dto;
 
 import lombok.Data;
+import java.io.Serializable;
 
 @Data
-public class CertificateRequestListDTO {
+public class CertificateRequestListDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Long requestId;
     private Long studentId;
     private String studentUsername;
     private Long courseId;
     private String courseName;
     private String status;
+
+    @Override
+    public String toString() {
+        return "Студентик {" +
+                "requestId=" + requestId +
+                ", studentUsername='" + studentUsername + '\'' +
+                ", courseName='" + courseName + '\'' +
+                '}';
+    }
 }
